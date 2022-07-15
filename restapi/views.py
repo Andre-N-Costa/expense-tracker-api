7418c11ca69e03fe0613b7d8b2f3d3eacedc9847
Andre-N-Costa <andrencosta2002@gmail.com>
2022-07-15 20:32:00 +0100

    added filtering

--- restapi/views.py
+++ restapi/views.py
@@ -5,10 +5,12 @@ from restapi import models, serializers
 from django.forms.models import model_to_dict
 from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 
+
 # Create your views here.
 class ExpenseListCreate(ListCreateAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
+    filterset_fields = ["category", "merchant"]
 
 
 class ExpenseRetrieveDelete(RetrieveDestroyAPIView):
